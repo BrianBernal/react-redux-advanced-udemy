e75b19bd326c205db6eeabdbf6555d08164bf8e5
Brian-LeanTech <bbernal@lean-tech.io>
2020-01-12 15:14:42 -0500

    CommentBox.test.js : Providing fake event to test change event of textarea

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,16 @@ it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 }); // Order execution: Second
+
+it('has a textarea that users can type in', () => {
+  //step 1. Find the textarea element
+  //step 2. Simulate a 'change' event in the element
+  wrapped.find('textarea').simulate('change', {
+    // el evento se refiere al contexto HTML, no al de jsx.
+    target: { value: 'new comment' }
+  });
+  // target hace referencia a e.target.value
+  // Es como si lanzaramos el evento dentro del cual
+  // se le da el value 'new comment'. Esto para evaluar
+  // el evento del textare (simulate the event)
+});
