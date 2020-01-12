8cf30f653cb045eebb9acaf873ff860328572429
Brian-LeanTech <bbernal@lean-tech.io>
2020-01-12 12:31:38 -0500

    App.test.js adding a second test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme'; // shallow render. See notebooks!
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
+const wrapped = shallow(<App />);
+/**
+ * wrapped para indicar que estamos trayendo es una
+ * versión wrapped de App. Así pues wrapped es un
+ * component, por lo que también sería valido llamarlo
+ * 'component'
+ */
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
-  /**
-   * wrapped para indicar que estamos trayendo es una
-   * versión wrapped de App. Así pues wrapped es un
-   * component, por lo que también sería valido llamarlo
-   * 'component'
-   */
-
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
